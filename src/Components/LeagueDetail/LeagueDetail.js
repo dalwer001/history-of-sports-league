885e1293bb06f36b9f2c5f30fa3352e483459f76
dalwer001 <dalwer.official@gmail.com>
2021-03-11 23:21:32 +0600

    add all

--- src/Components/LeagueDetail/LeagueDetail.js
+++ src/Components/LeagueDetail/LeagueDetail.js
@@ -49,7 +49,7 @@ const {strLogo,strLeague,intFormedYear,strCountry,strSport,strGender,strDescript
                 </div>
             </div>
 
-            <div className="container text-white mb-3 card-font mt-4">
+            <div className="container text-white mb-3 card-font mt-4 p-4">
                 <div style={{ width: '100%' }}>
                     <div className="row  row-detail">
                         <div className="col-md-8 col-sm-12">
